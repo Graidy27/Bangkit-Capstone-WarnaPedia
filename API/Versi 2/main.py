e076ca7031636e87d4e7312e49d2e0aa1f456313
Graidy27 <graidyits@gmail.com>
2023-06-14 16:55:44 +0700

    Fix bug in main.py and add gitignore file

--- API/Versi 2/main.py
+++ API/Versi 2/main.py
@@ -31,5 +31,5 @@ def request_page():
 
 
 if __name__ == '__main__':
-    app.run(port="7777")
+    app.run(host='0.0.0.0', port=7777)
     # app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT, 8080")))
